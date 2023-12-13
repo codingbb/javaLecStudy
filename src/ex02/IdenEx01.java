97eecb0a6970864da4da0475c86aaa3045f0da4a
SCHcoding <lana_75@naver.com>
2023-12-13 12:40:22 +0900

    static 공부

---
+++ src/ex02/IdenEx01.java
@@ -0,0 +1,17 @@
+package ex02;
+
+public class IdenEx01 {
+    public static void main(String[] args) {
+        /**
+         * 변수 작성법
+         * 1. 카멜(낙타) 표기법을 사용한다.
+         * 2. 숫자를 먼저 사용하지 않는다.
+         * 3. 특수문자는 사용하지 않는다.
+         */
+        int user_money; //절대 사용안함!!
+        int userMoney; //낙타(카멜) 표기법
+    //    int abc#;
+        double abc$$;   //특수문자 사용하지 마라
+    //    int 1stMoney;   //숫자로 변수를 시작할 수 없다.
+    }
+}
---
+++ src/ex04/MemEx02.java
@@ -0,0 +1,7 @@
+package ex04;
+
+public class MemEx02 {
+    public static void main(String[] args) {
+
+    }
+}
