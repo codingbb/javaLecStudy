c7b6f1e0ed96edcf75778ec18bc0edc1625e7e96
SCHcoding <lana_75@naver.com>
2023-12-19 09:19:01 +0900

    객체

---
+++ src/ex04/Circle.java
@@ -0,0 +1,15 @@
+package ex04;
+
+//설계도
+public class Circle {
+    private int radius;
+
+    public  Circle() {
+
+    }
+
+    public double getArea() {
+        return 3.14 * radius * radius;
+    }
+
+}
