61cbbb7eb7340d70f971d054602a98c074d5a15e
SCHcoding <lana_75@naver.com>
2023-12-20 11:34:10 +0900

    구구단 ㄹ리팩토링

---
+++ src/ex03/test/GugudanEx04.java
@@ -0,0 +1,17 @@
+package ex03.test;
+
+class GugudanUtil {
+    static void gugudan(int x) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(x + "*" + i + "=" + (x * i));
+        }
+        System.out.println();
+    }
+}
+
+public class GugudanEx04 {
+
+    public static void main(String[] args) {
+        GugudanUtil.gugudan(10);
+    }
+}
