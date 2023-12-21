dc0a6e6d1c966b0c86715b29c88e8c9eaa79df8f
SCHcoding <lana_75@naver.com>
2023-12-21 14:40:46 +0900

    bankApp 완성 - 트랜젝션 + 객체ì의 상태변경, 객체의 상태확인

--- src/ex04/example2/BankService.java
+++ src/ex04/example2/BankService.java
@@ -5,9 +5,31 @@ import ex04.example2.model.Account;
 //트랜잭션 관리
 public class BankService {
 
+    public static void 출금(Account withdrawAccount, long amount) {
+        //출금의 트랜지션 (과정 3개)
+        //0원 검증
+        if(amount <= 0) {
+            System.out.println("0원 이하 금액 출금 불가능");
+            return;
+        }
+        if(withdrawAccount.잔액부족하니(amount)){
+            System.out.println("잔액이 부족합니다");
+            return;
+        }
+        withdrawAccount.출금(amount);
+    }
+
+
     public static void 이체(Account senderAccount, Account receiverAccount, long amount) {    //타입으로 객체를 전달받음!!
+        //이 코드는 상태를 확인하는 것도, 변경하는 것도 아님 (검증하는 것 = 벨리데이션?)
+        //이게 이체 트랜지션
         if(amount <= 0) {
             System.out.println("0원 이하 금액 이체 불가능");
+            return;
+        }
+
+        if(senderAccount.잔액부족하니(amount)){
+            System.out.println("잔액이 부족합니다");
             return;
         }
 
--- src/ex04/example2/model/Account.java
+++ src/ex04/example2/model/Account.java
@@ -1,9 +1,18 @@
 package ex04.example2.model;
 
+//객체의 상태를 변경, 객체의 상태를 확인
 public class Account {
     private final int id;
     private long balance;
     private int userId; //account id랑 구분하기 위해서 userId
+
+    //긍정으로 물어봐서 내가 원하는 결과를 true로 받자
+    public boolean 잔액부족하니(long amount) {
+        if(balance < amount) {
+            return true;
+        }
+        return false;
+    }
 
     //메서드는 하나의 일(책임)만 하게 만들기. 애는 출금이니까 출금!!하는 기능만.
     //만약 이체라고 했으면 출금도 해야하고 입금도 해야하니까 나중에 문제가 생겼을때 출금이 잘못된건지 입금이 잘못된건지 구분하기가 어렵다.
